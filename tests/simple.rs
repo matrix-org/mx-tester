9854e2546537f0ee561453a996ebddcba00e1ec8
David Teller <d.o.teller+github@gmail.com>
2021-10-18 15:24:23 +0200

    [CI] Now testing that (only) admin users can use the admin API

--- tests/simple.rs
+++ tests/simple.rs
@@ -1,6 +1,5 @@
-use std::convert::TryFrom;
-
 use mx_tester::{self, registration::User, *};
+use std::{convert::TryFrom, ops::Not};
 
 #[tokio::test]
 async fn test_create_users() {
@@ -44,18 +43,22 @@ async fn test_create_users() {
     // Now attempt to login as our users.
     let homeserver_url = reqwest::Url::parse(&config.homeserver_config.public_baseurl).unwrap();
 
-    let admin_client = matrix_sdk::Client::new(homeserver_url.clone()).unwrap();
-    admin_client
-        .login(&admin.localname, &admin.password, None, None)
-        .await
-        .expect("Could not login as admin");
-    // FIXME: Check that we can use an admin API.
-
     let regular_user_client = matrix_sdk::Client::new(homeserver_url.clone()).unwrap();
     regular_user_client
         .login(&regular_user.localname, &regular_user.password, None, None)
         .await
         .expect("Could not login as regular user");
+    let regular_user_id = regular_user_client
+        .whoami()
+        .await
+        .expect("Could not request whoami for regular user")
+        .user_id;
+    assert!(
+        regular_user_id.as_str().contains(&regular_user.localname),
+        "Expected to find local name {} in user_id {}",
+        regular_user.localname,
+        regular_user_id
+    );
 
     let regular_user_client_with_custom_password =
         matrix_sdk::Client::new(homeserver_url.clone()).unwrap();
@@ -68,6 +71,57 @@ async fn test_create_users() {
         )
         .await
         .expect("Could not login as regular user");
+    let regular_user_client_with_custom_password_user_id = regular_user_client_with_custom_password
+        .whoami()
+        .await
+        .expect("Could not request whoami for regular user")
+        .user_id;
+    assert!(
+        regular_user_client_with_custom_password_user_id
+            .as_str()
+            .contains(&regular_user_with_custom_password.localname),
+        "Expected to find local name {} in user_id {}",
+        regular_user_with_custom_password.localname,
+        regular_user_client_with_custom_password_user_id
+    );
+
+    let admin_client = matrix_sdk::Client::new(homeserver_url.clone()).unwrap();
+    admin_client
+        .login(&admin.localname, &admin.password, None, None)
+        .await
+        .expect("Could not login as admin");
+    let admin_user_id = admin_client
+        .whoami()
+        .await
+        .expect("Could not request whoami for admin")
+        .user_id;
+    assert!(
+        admin_user_id.as_str().contains(&admin.localname),
+        "Expected to find local name {} in user_id {}",
+        admin.localname,
+        admin_user_id
+    );
+
+    // Now check whether the admin can use the user API and others can't.
+    let request = synapse_admin_api::users::get_details::v2::Request::new(&regular_user_id);
+    let response = admin_client
+        .send(request, None)
+        .await
+        .expect("Admin could not request user details");
+    assert!(response.details.admin.not());
+    assert!(response.details.deactivated.not());
+    assert_eq!(response.details.displayname, regular_user.localname);
+
+    for client in [
+        &regular_user_client,
+        &regular_user_client_with_custom_password,
+    ] {
+        let request = synapse_admin_api::users::get_details::v2::Request::new(&regular_user_id);
+        client
+            .send(request, None)
+            .await
+            .expect_err("A non-admin user should not be able to send an admin API request");
+    }
 
     mx_tester::down(
         &SynapseVersion::ReleasedDockerImage,
