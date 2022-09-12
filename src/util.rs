d8f29444d1e8dcdca80fb2b4a266297199175518
David Teller <d.o.teller+github@gmail.com>
2022-09-12 13:07:32 +0200

    Increasing default rate limits

--- src/util.rs
+++ src/util.rs
@@ -1,3 +1,7 @@
+use async_trait::async_trait;
+use log::debug;
+use rand::Rng;
+
 /// A generic syntax for dict-like structures.
 ///
 /// Works for HashMap but also for e.g. serde_json or serde_yaml maps.
@@ -222,6 +226,52 @@ impl AsRumaError for matrix_sdk::Error {
         match *self {
             matrix_sdk::Error::Http(ref err) => err.as_ruma_error(),
             _ => None,
+        }
+    }
+}
+
+#[async_trait]
+pub trait Retry {
+    async fn auto_retry(&self, attempts: u64) -> Result<reqwest::Response, anyhow::Error>;
+}
+
+#[async_trait]
+impl Retry for reqwest::RequestBuilder {
+    async fn auto_retry(&self, max_attempts: u64) -> Result<reqwest::Response, anyhow::Error> {
+        /// The duration of the retry will be picked randomly within this interval,
+        /// plus an exponential backoff.
+        const BASE_INTERVAL_MS: std::ops::Range<u64> = 300..1000;
+
+        let mut attempt = 1;
+        loop {
+            match self
+                .try_clone()
+                .expect("Cannot auto-retry non-clonable requests")
+                .send()
+                .await
+            {
+                Ok(response) => {
+                    debug!("auto_retry success");
+                    break Ok(response);
+                }
+                Err(err) => {
+                    debug!("auto_retry error {:?} => {:?}", err, err.status());
+                    // FIXME: Is this the right way to decide when to retry?
+                    let should_retry = attempt < max_attempts
+                        && (err.is_connect() || err.is_timeout() || err.is_request());
+
+                    if should_retry {
+                        let duration =
+                            (attempt * attempt) * rand::thread_rng().gen_range(BASE_INTERVAL_MS);
+                        attempt += 1;
+                        debug!("auto_retry: sleeping {}ms", duration);
+                        tokio::time::sleep(std::time::Duration::from_millis(duration)).await;
+                    } else {
+                        debug!("auto_retry: giving up!");
+                        return Err(err.into());
+                    }
+                }
+            }
         }
     }
 }
